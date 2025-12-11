b5b25169d60d03571371f3fb50e7588375c901c0
Samuele Cozzi <samuele.cozzi@gmail.com>
2025-12-11 22:20:05 +0100

    feat: Configure Next.js for static export and update Firebase deployment to serve the generated static assets.

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
@@ -7,7 +8,10 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 });
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",       // <-- REQUIRED for static hosting
+  images: {
+    unoptimized: true,    // <-- Required for next export when using next/image
+  },
   turbopack: {},
 };
 
